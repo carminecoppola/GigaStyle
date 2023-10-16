227d96050c671051ff54255bdc819bef8f7af010
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-10-16 20:36:34 +0200

    commit

--- app.py
+++ app.py
@@ -3,17 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/index')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/reservation')
+@app.route('/reservation/')
 def reservation():
     return render_template('reservation.html')
 

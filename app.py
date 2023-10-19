67b34902810aa8223ce18a966ec8a9233935288f
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-10-19 10:31:44 +0200

    commit

--- app.py
+++ app.py
@@ -1,13 +1,54 @@
-from flask import Flask, render_template
+from functools import wraps
+
+from flask import Flask, render_template, session, redirect, flash, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 # Connessione a un'istanza MongoDB locale
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb+srv://miniello917:<password>@gigabarber.jqoknjr.mongodb.net/?retryWrites=true&w=majority")
 
 # Specifica il database a cui connettersi se non esiste lo crea (Collezzione ???)
 db = client['db-gigabarber']
+userCollection = db["users"]
+
+
+# Decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
+
+
+def login_not_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return redirect('/')
+        else:
+            return f(*args, **kwargs)
+
+    return wrap
+
+
+def admin_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if session['user']['role'] == "admin":
+            return f(*args, **kwargs)
+        else:
+            flash("You need to be an admin to view this page.")
+            return redirect(url_for('home'))
+
+    return wrap
+
+
+# Routes
 
 
 @app.route('/')
@@ -18,6 +59,7 @@ def index():
 @app.route('/reservation/')
 def reservation():
     return render_template('reservation.html')
+
 
 @app.route('/login/')
 def login():

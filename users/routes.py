02db230a81983b4253cbcb0c46f15638432de437
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-10-19 10:31:44 +0200

    commit

---
+++ users/routes.py
@@ -0,0 +1,17 @@
+from app import app
+from users.models import User
+
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
+    return User().signup()
+
+
+@app.route('/user/signout')
+def signout():
+    return User().signout()
+
+
+@app.route('/user/login', methods=['POST'])
+def login2():
+    return User().login()

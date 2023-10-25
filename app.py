c846bca7258853b4122d2b79273c7f567841edd9
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-10-25 21:35:37 +0200

    funzionalaregistrazioneeillogin

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+import secrets
+
+from flask import Flask, request, render_template, redirect, url_for, session
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
 uri = "mongodb+srv://miniello:pericle@cluster0.1lorjnu.mongodb.net/?retryWrites=true&w=majority"
 
@@ -15,26 +18,14 @@ try:
 except Exception as e:
     print(e)
 
-db = client.DBUtenti
-coll = db.utenti
+db = client['utenti']
+collection = db['utenti']
 
 
 # Routes
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/registration/', methods=['GET', 'POST'])
-def registration():
-    print('Africani bastardi')
-    return render_template('registration.html')
-
-
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    print('Luridi Africani bastardi')
-    return render_template('login.html')
 
 
 @app.route('/reservation/')
@@ -44,7 +35,6 @@ def reservation():
 
 @app.route('/choose/')
 def choose():
-    print('Choose fratm ennar')
     return render_template('choose.html')
 
 
@@ -56,6 +46,65 @@ def barber():
 @app.route('/hairdresser/')
 def hairdresser():
     return render_template('reservationHairDresser.html')
+
+
+@app.route('/registrazione', methods=['GET', 'POST'])
+def registrazione():
+    if request.method == 'POST':
+        # Ottieni i dati del modulo di registrazione
+        email = request.form['email']
+        password = request.form['password']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone = request.form['phone']
+        gender = request.form['gender']
+
+        # Verifica se l'utente esiste già
+        if collection.find_one({'email': email}):
+            return 'Questa email esiste già. Scegli un altra email.'
+
+        # Inserisci l'utente nel database con tutti i campi
+        new_user = {
+            'email': email,
+            'password': password,
+            'first_name': first_name,
+            'last_name': last_name,
+            'phone': phone,
+            'gender': gender
+        }
+        collection.insert_one(new_user)
+
+        # Reindirizza l'utente alla pagina "choose.html" dopo la registrazione
+        return redirect(url_for('choose'))
+
+    return render_template('registration.html')
+
+
+# Pagina di login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = collection.find_one({'email': email, 'password': password})
+
+        if user:
+            # Login riuscito, creiamo una sessione
+            session['email'] = email
+            # Reindirizza l'utente alla pagina "choose.html" e passa l'informazione dell'email come variabile
+            return redirect(url_for('choose', email=email))
+
+        else:
+            return 'Credenziali errate. Riprova o <a href="/registrazione">registrati</a>.'
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return 'Sei stato disconnesso. <a href="/">Torna alla pagina principale</a>'
 
 
 if __name__ == '__main__':
